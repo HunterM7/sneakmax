951643fb3cca8e2994fa74d8e96f30a11f96c2dc
HunterM7 <tonym7@yandex.ru>
2022-04-20 18:40:44 +0300

    first commit

---
+++ gulp/config/path.js
@@ -0,0 +1,40 @@
+// Получаем имя папки проекта
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = `./dist`;
+const srcFolder = `./src`;
+
+export const path = {
+	build: {
+		html: `${buildFolder}/`,
+		css: `${buildFolder}/css/`,
+		js: `${buildFolder}/js/`,
+		images: `${buildFolder}/img/`,
+		fonts: `${buildFolder}/fonts/`,
+		files: `${buildFolder}/files/`,
+	},
+	src: {
+		html: `${srcFolder}/*.pug`,
+		sass: `${srcFolder}/sass/style.sass`,
+		scss: `${srcFolder}/scss/style.scss`,
+		js: `${srcFolder}/js/app.js`,
+		svg: `${srcFolder}/img/**/*.svg`,
+		svgicons: `${srcFolder}/svgicons/*.svg`,
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+		files: `${srcFolder}/files/**/*.*`,
+	},
+	watch: {
+		html: `${srcFolder}/**/*.pug`,
+		sass: `${srcFolder}/sass/**/*.sass`,
+		scss: `${srcFolder}/scss/**/*.scss`,
+		js: `${srcFolder}/js/**/*.js`,
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,svg,ico}`,
+		files: `${srcFolder}/files/**/*.*`,
+	},
+	clean: buildFolder,
+	buildFolder: buildFolder,
+	srcFolder: srcFolder,
+	rootFolder: rootFolder,
+	ftp: `test`
+}
